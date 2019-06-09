bc458ee24bf8641f1717765d11d4b708ca52f999
Konstantin <barrbozzopower@gmail.com>
2019-06-09 19:25:28 +0400

    Install bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+
 import "../assets/App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      Hello world
+      <Button variant={"info"}>Bootstrap Btn</Button>
+    </div>
+  );
 }
 
 export default App;
