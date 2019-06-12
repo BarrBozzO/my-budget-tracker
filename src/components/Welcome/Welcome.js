71637c6b0dd00fda67765ab9188bc76b9ee71d98
Konstantin <barrbozzopower@gmail.com>
2019-06-12 18:53:35 +0400

    Feat: create welcome component

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import { Button } from "react-bootstrap";
+
+import styles from "./Welcome.module.scss";
+
+function Welcome() {
+  return (
+    <div
+      className={classNames(styles.welcome, "container-fluid", "text-center")}
+    >
+      <h1 className={classNames(styles.welcome__title)}>My Budget Tracker</h1>
+      <span className={classNames(styles.welcome__message)}>
+        Manage your finance easy!
+      </span>
+      <div className={"m-3"}>
+        <Button className={classNames(styles.welcome__button, "mx-2")}>
+          Sign In
+        </Button>
+        <Button className={classNames(styles.welcome__button, "mx-2")}>
+          Sign Up
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
