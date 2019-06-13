f0d2b7fd1d8aabcbb45acdd7bd0421eef5f5c9d4
Konstantin <barrbozzopower@gmail.com>
2019-06-13 21:52:38 +0400

    Feat: Add Accounts Component

--- src/components/Accounts/Accounts.js
+++ src/components/Accounts/Accounts.js
@@ -1,5 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
-export default function Accounts() {
-  return <div>Accounts</div>;
+import styles from "./Accounts.module.scss";
+
+function Accounts() {
+  return (
+    <div className={classNames("container-fluid", styles.accounts)}>
+      <div className="row">
+        <div className="col-lg-4 col-md-6 col-sm-12">
+          <div className={styles.accounts__card}>
+            <div className={styles["accounts__card-title"]}>Account name</div>
+            <div className={styles["accounts__card-content"]} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Accounts;
