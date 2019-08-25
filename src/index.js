2ce528a30f3976487de35e3972cea189a2ea3cb8
Konstantin <barrbozzopower@gmail.com>
2019-08-25 08:10:34 +0400

    add store persistance

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./assets/scss/index.scss";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
-import store from "./store/store";
+import store, { persistor } from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,27 @@
 import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 import rootSaga from "./sagas";
 import rootReducer from "./reducers";
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
+const store = createStore(
+  persistedReducer,
+  applyMiddleware(sagaMiddleware, logger)
+);
+export const persistor = persistStore(store);
 
 // start saga
 sagaMiddleware.run(rootSaga);
