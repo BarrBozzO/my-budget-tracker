9162cae3245807bbfc5e1c5ada1dffbc4122a871
Konstantin <barr.develop@gmail.com>
2020-02-04 00:49:47 +0400

    update ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,8 @@ import classNames from "classnames";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
 
+import { ReactComponent as AvatarPlaceholder } from "assets/svg/user-avatar-placeholder.svg";
+
 import styles from "./Header.module.scss";
 
 function Header(props) {
@@ -15,10 +17,9 @@ function Header(props) {
         "p-3"
       )}
     >
-      <div
-        className={styles.header__user}
-        style={{ backgroundImage: "url('/')" }}
-      />
+      <div className={styles.header__user}>
+        <AvatarPlaceholder fill="#999999" />
+      </div>
       <div className={styles["header__button-container"]}>
         <Button
           variant="link"
