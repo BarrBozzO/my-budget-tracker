d232d016f4bc777cb8a7fe703361ffefeb319b2e
Konstantin <barr.develop@gmail.com>
2020-02-11 10:20:57 +0400

    1. add formatNumber util function 2. add accountStatuses constants

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,7 @@ export const capitalize = string => {
     ? string.charAt(0).toUpperCase() + string.slice(1)
     : null;
 };
+
+export const formatNumber = num => {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+};
